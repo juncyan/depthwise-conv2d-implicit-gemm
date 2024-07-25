ad631e6a03752580133b6f2374dfff9107cb6cf4
juncyan <hjq.r@outlook.com>
2024-07-26 00:06:53 +0800

    20240726

--- main.py
+++ main.py
@@ -40,12 +40,12 @@ num_classes = 2
 # model = DeepLabV3P(num_classes, backbone=ResNet50_vd(in_channels=6))
 # model = SegNeXt(num_classes=num_classes, decoder_cfg={}, backbone=ResNet50_vd(in_channels=6))
 # model = DSAMNet(3,2)
-# model = MSFGNet(3, 2)
+model = MSFGNet(3, 2)
 # model = P2V(3,2)
 # model = FCCDN(3,2)
 # model = FCSiamConc(3,2)
 # model = SNUNet(3, 2)
-model = F3Net()
+# model = F3Net()
 
 
 model_name = model.__str__().split("(")[0]

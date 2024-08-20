d0eea8e4e6282f2c99de1e84b8b53dd099219544
juncyan <hjq.r@outlook.com>
2024-08-20 14:31:12 +0800

    init samcd 2

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from paddleseg.models import layers
 from paddleseg.models.losses import lovasz_loss
 
 
-from models.SegmentAnything.segment_anything.build_sam import build_sam_vit_t
+from models.segment_anything.build_sam import build_sam_vit_t
 x = paddle.randn([1, 64, 256, 256]).cuda()
 pd_path = r"/home/jq/Code/weights/vit_t.pdparams"
 m = build_sam_vit_t(checkpoint=pd_path).to('gpu')
